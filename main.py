b5efa730eb434be66571b69b2b7659fbd8e90348
Jluzano <justinluzano@gmail.com>
2023-05-14 16:35:53 -0700

    trying out movement

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 import random
 
+# List of boids
+boids = []
+
 class Boid:
     def __init__(self, x, y, size, canvas):
         self.x = x
@@ -10,6 +13,16 @@ class Boid:
         # Top left corner of the oval at (x - size, y - size), bottom right corner at (x + size, y + size)
         self.circle = canvas.create_oval(x - size, y - size, x + size, y + size, fill='white')
 
+    # Movement function for the boid
+    def move(self):
+        self.x += self.x
+        self.y += self.y
+
+def update():
+    for boid in boids:
+        boid.move()
+    canvas.after(10, update)
+
 # Creating the canvas
 root = tk.Tk()
 # Canvas is 800 x 600
@@ -17,7 +30,6 @@ canvas = tk.Canvas(root, width = 800, height = 600, bg='black')
 canvas.pack()
 
 # Creating a number of boids
-boids = []
 for i in range(10):
     # Spawns boids in random places on the canvas
     x = random.randint(0, 800)
@@ -26,4 +38,5 @@ for i in range(10):
     boid = Boid(x, y, 10, canvas)
     boids.append(boid)
 
+update()
 root.mainloop()
