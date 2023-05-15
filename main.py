706207ba4454f8dcff6ef24bc35f49c99e3fa568
Jluzano <justinluzano@gmail.com>
2023-05-14 21:37:01 -0700

    added rough alignment (needs fixing)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import random
+import math
 
 # List of boids
 boids = []
@@ -28,10 +29,32 @@ class Boid:
             self.speedy = -self.speedy
         # Updates the boid on the canvas
         self.canvas.move(self.circle, self.speedx, self.speedy)
+    
+    def alignment(self):
+        avgx = 0
+        avgy = 0
+        count = 0
+        for boid in boids:
+            # Calculates the distance between itself and other boids around it
+            distance = math.dist([self.x, self.y], [boid.x, boid.y])
+            if boid != self and distance < 50:
+                avgx += boid.speedx
+                avgy += boid.speedy
+                count += 1
+        # Calculates the average speed and assigns it to the boid
+        if count > 0:
+            avgx /= count
+            avgy /= count
+            self.speedx = avgx
+            self.speedy = avgy
+                
+
+        
 
 def update():
     # Constantly calls the move function to move the boids
     for boid in boids:
+        boid.alignment()
         boid.move()
     canvas.after(10, update)
 
@@ -46,6 +69,7 @@ for i in range(10):
     # Spawns boids in random places on the canvas
     x = random.randint(0, 800)
     y = random.randint(0, 600)
+    # Set a random velocity to each boid
     randx = random.randint(3, 5)
     randy = random.randint(3, 5)
     # Determining the size of the boid
